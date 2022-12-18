66b38697f869c00f75fa3d483cc7550fb19e16be
gimmicks-u <is@pusan.ac.kr>
2022-12-19 00:13:29 +0900

    🐛 fix: presingedURL api 엔드포인트, 응답코드 변경
    endpoint /presigned-url -> /photos/presigned-url
    statuscode 201 -> 200

--- src/presignedURL/presigned-url.controller.ts
+++ src/presignedURL/presigned-url.controller.ts
@@ -1,19 +1,11 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  Post,
-  Query,
-  UseFilters,
-} from '@nestjs/common';
+import { Controller, Get, HttpCode, Query, UseFilters } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { HttpExceptionFilter } from 'src/common/exceptions/httpException.filter';
 import { PresignedUrlDto } from './dto/presigned-url.dto';
 import { PresignedUrlService } from './presigned-url.service';
 
 @ApiTags('이미지 업로드')
-@Controller('presigned-url')
+@Controller('photos')
 @UseFilters(HttpExceptionFilter)
 export class PresignedUrlController {
   constructor(private readonly presignedUrlService: PresignedUrlService) {}
@@ -22,8 +14,8 @@ export class PresignedUrlController {
     summary: 'presignedURL 요청',
     description: 'query 파라미터로 filetype을 입력해서 presignedURL을 요청함',
   })
-  @Get()
-  @HttpCode(201)
+  @Get('presigned-url')
+  @HttpCode(200)
   presignedUrl(@Query() query: PresignedUrlDto) {
     const { filetype } = query;
     return this.presignedUrlService.presignedUrl(filetype);
