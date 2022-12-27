7779f51ba4f01ab1758c389e21e8fed34282ef48
jaehun <skybloom7777@gmail.com>
2022-12-27 21:22:50 +0900

    📝 docs: Swagger 수정작업

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,15 +1,7 @@
-import {
-  Controller,
-  Param /*UseGuards */,
-  ParseIntPipe,
-  Put,
-} from '@nestjs/common';
-import { /*Post,*/ Get /*, Body*/ } from '@nestjs/common';
+import { Controller, Param, ParseIntPipe, Put } from '@nestjs/common';
+import { Get } from '@nestjs/common';
 import { CurrentUser } from 'src/common/decorators/user.decorator';
-// import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
-// import { CurrentUser } from 'src/common/decorators/user.decorator';
 import { Profile } from 'src/entities';
-import { UsersService } from 'src/users/users.service';
 import { ProfilesService } from './profiles.service';
 import { UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
@@ -23,10 +15,7 @@ import {
 @ApiTags('프로필 API')
 @Controller('profiles')
 export class ProfilesController {
-  constructor(
-    private readonly usersService: UsersService,
-    private readonly profilesService: ProfilesService,
-  ) {}
+  constructor(private readonly profilesService: ProfilesService) {}
 
   @ApiOperation({
     summary: '유저 프로필 사진 수정',
@@ -41,22 +30,7 @@ export class ProfilesController {
           {
             profile_nickname: 'test12',
             user_email: 'test12@test.com',
-            image_url: 'http://test.com',
-          },
-        ],
-      },
-    },
-  })
-  @ApiResponse({
-    status: 200,
-    schema: {
-      example: {
-        success: true,
-        data: [
-          {
-            profile_nickname: 'test1234',
-            user_email: 'test1234@test.com',
-            image_url: null,
+            image_url: 'http://test.com | null',
           },
         ],
       },
